b00361d27420cf18844dd71ed120092447f95bb7
Jason Volk <jason@zemos.net>
2025-08-04 03:29:56 +0000

    Add specific shorteventid to shortstatehash query function.
    Signed-off-by: Jason Volk <jason@zemos.net>

--- src/service/rooms/state_accessor/state.rs
+++ src/service/rooms/state_accessor/state.rs
@@ -420,19 +420,24 @@ async fn load_full_state(&self, shortstatehash: ShortStateHash) -> Result<Arc<Co
 		.await
 }
 
-/// Returns the state hash for this pdu.
+/// Returns the state hash at this event.
 #[implement(super::Service)]
 pub async fn pdu_shortstatehash(&self, event_id: &EventId) -> Result<ShortStateHash> {
-	const BUFSIZE: usize = size_of::<ShortEventId>();
-
 	self.services
 		.short
 		.get_shorteventid(event_id)
-		.and_then(|shorteventid| {
-			self.db
-				.shorteventid_shortstatehash
-				.aqry::<BUFSIZE, _>(&shorteventid)
-		})
+		.and_then(|shorteventid| self.get_shortstatehash(shorteventid))
+		.await
+}
+
+/// Returns the state hash at this event.
+#[implement(super::Service)]
+pub async fn get_shortstatehash(&self, shorteventid: ShortEventId) -> Result<ShortStateHash> {
+	const BUFSIZE: usize = size_of::<ShortEventId>();
+
+	self.db
+		.shorteventid_shortstatehash
+		.aqry::<BUFSIZE, _>(&shorteventid)
 		.await
 		.deserialized()
 }
