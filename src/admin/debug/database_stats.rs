9a4b5b020902c5dda124f6d40799369988c2018e
Jason Volk <jason@zemos.net>
2026-08-03 01:29:57 +0000

    admin/debug: Error per-column rather than panic on invalid property name.
    Signed-off-by: Jason Volk <jason@zemos.net>

--- src/admin/debug/database_stats.rs
+++ src/admin/debug/database_stats.rs
@@ -20,7 +20,10 @@ pub(super) async fn database_stats(
 		.filter(|&(&name, _)| map_name.is_empty() || map_name == name)
 		.try_stream()
 		.try_for_each(|(&name, map)| {
-			let res = map.property(&property).expect("invalid property");
+			let res = map
+				.property(&property)
+				.unwrap_or_else(|e| format!("invalid property: {e}"));
+
 			writeln!(self, "##### {name}:\n```\n{}\n```", res.trim())
 		})
 		.await
